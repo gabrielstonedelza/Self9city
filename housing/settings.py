f1e2baebf97a07d07d7601ff5dda03a2bad0a26d
gabrielstonedelza <gabrielstonedelza@gmail.com>
2021-08-08 19:29:37 +0000

    add server address

--- housing/settings.py
+++ housing/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['142.93.201.16', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['64.227.22.163', 'localhost', '127.0.0.1']
 
 # Application definition
 
